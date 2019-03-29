a2f1693d6bb7132d1fb07464f682e7f5ab55a618
saikiran s <saikiran.salla@gmail.com>
2019-03-29 00:57:44 -0500

    navigation to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,24 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import AuthPage from './pages/Auth'
 import Bookings from './pages/Bookings'
 import Events from './pages/Event'
+import MainNavigation from './components/Navigation/MainNavigation'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact/>
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={Events} />
-          <Route path="/bookings" component={Bookings} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+            <main className="main-content">
+              <Switch>
+                <Redirect from="/" to="/auth" exact/>
+                <Route path="/auth" component={AuthPage} />
+                <Route path="/events" component={Events} />
+                <Route path="/bookings" component={Bookings} />
+              </Switch>
+            </main>
+          </React.Fragment>
       </BrowserRouter>
     );
   }
