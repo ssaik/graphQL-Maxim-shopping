403787d6d622ce1d6b2869b67a021ed17fa141b0
saikiran s <saikiran.salla@gmail.com>
2019-04-01 21:29:40 -0500

    added auth to the MongoDB

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if(req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(
   '/graphql',
   graphqlHttp({
@@ -24,7 +34,7 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ssaikgraphqlcluster-rnku5.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(4000);
   })
   .catch(err => {
     console.log(err);
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -2,20 +2,68 @@ import React, { Component } from 'react';
 import './Auth.css'
 
 class Authpage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();     //this means it prevents all the other actions except Submit, i.e. Default.
+        const email = this.emailEl.current.value;
+        const password = this.passwordEl.current.value;
+
+        if(email.trim().length === 0 || password.trim().length === 0 ) {
+            return;
+        }
+
+        const requestBody = {
+            query: `
+                mutation {
+                    createUser(userInput: {email: "${email}", password: "${password}"}) {
+                        _id
+                        email
+                    }
+                }
+            `
+        };
+
+        fetch('http://localhost:4000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            if(res.status !== 200 && res.status !== 201) {
+                throw new Error('Failed!');
+            }
+            return res.json();
+        })
+        .then(resData => {
+            console.log(resData);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+        console.log(email, password);
+    }
+
     render () {
         return (
-        <form className="auth-form">
+        <form className="auth-form" onSubmit={this.submitHandler}>
             <div className="form-control">
                 <label htmlFor="email">E-Mail</label>
-                <input type="email" id="email" />
+                <input type="email" id="email" ref={this.emailEl} />    {/*This will automatically validates*/}
             </div>
             <div className="form-control">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
+                <input type="password" id="password" ref={this.passwordEl} />
             </div>
             <div className="form-actions">
+                <button type="submit">Submit</button>
                 <button type="button">Switch to Signup</button>
-                <button type="button">Submit</button>
             </div>
         </form>
         )
